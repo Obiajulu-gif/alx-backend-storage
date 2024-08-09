47fbd332faeaf697b9232e1b21281823e74bb86a
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-09 22:24:21 +0100

    task 7 done

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,17 @@
 -- 7. Average score
 
 DELIMITER $$
+-- Comment: Query Description
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score DECIMAL(10, 2);
 
-    -- Calculate the average score for the given user_id
+-- Calculate the average score for the given user_id
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Update the average_score in the users table for the given user_id
+-- Update the average_score in the users table for the given user_id
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
