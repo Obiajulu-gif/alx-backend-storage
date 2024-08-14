5e4346c1f79b658a768c2b1c966bea4fc31fecb9
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-14 23:48:17 +0100

    task 1 done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Exercise file
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -19,3 +19,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str,
+                                                    bytes,
+                                                    int,
+                                                    float,
+                                                    None]:
+        """ Retrieve data from Redis and convert it using the
+        provided function"""
+        value = self._redis.get(key)
+        if value is not None and fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve a string from Redis."""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve an integer from Redis"""
+        value = self.get(key)
+        return int(value) if value is not None else None
